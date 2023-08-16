9bda09ba5e4d023ad616f8adfd69ae52f511efc4
Azura <saamueel64@outlook.es>
2023-08-15 18:13:21 -0600

    Implemented comprehensive database functionality with putDb and getDb methods. The putDb method updates the database with new content ensuring accuarate data storage. The getDb method retrieves content from the data base, promoting effective data retrieval and utilization.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,71 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+
   openDB('jate', 1, {
+
     upgrade(db) {
+
       if (db.objectStoreNames.contains('jate')) {
+
         console.log('jate database already exists');
         return;
+
       }
+
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
+
     },
+
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// This method is used to update the database with new content.
+export const putDb = async (content) => {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  console.log('Updating the database with new content!');
+
+  // Opens the database.
+  const db = await openDB('jate', 1);
+
+  // Creates a transaction.
+  const tx = db.transaction('jate', 'readwrite');
+
+  // Gets the object store.
+  const store = tx.objectStore('jate');
+
+  // Adds the content to the database.
+  const request = store.put({id: 1, value: content});
+
+  // Waits for the transaction to complete.
+  const result = await request;
+  console.log('Database updated successfully!', result);
+
+};
+
+// This method is used to get the content from the database.
+export const getDb = async () => {
+
+  console.log('Getting the database content!');
+
+  // Opens the database.
+  const db = await openDB('jate', 1);
+
+  // Creates a transaction.
+  const tx = db.transaction('jate', 'readonly');
+
+  // Gets the object store.
+  const store = tx.objectStore('jate');
+
+  // Gets the content from the database.
+  const request = store.get(1);
+
+  // Waits for the transaction to complete.
+  const result = await request;
+  console.log('Database content retrieved successfully!', result);
+
+  return result.value;
+
+}
 
 initdb();
