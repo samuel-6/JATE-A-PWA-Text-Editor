9eee9d26f10ae27a94be2649489e012061ca49bf
Azura <saamueel64@outlook.es>
2023-08-15 18:04:02 -0600

    Configured HTMLWebpackPlugin for dynamic HTML generation, integrated InjectManifest to set up the service worker, implemented WebpackPwaManifest for PWA manifest generation, and included the loaders and presets for CSS and JavaScript modules.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generates an HTML file from a template.
+      // It allows dynamic injection of generated bundles into the generated HTML file.
+      new HtmlWebpackPlugin({
+        template: './index.html', // Template path.
+        title: 'JATE', // Title of the generated HTML file.
+      }),
+      // Sets up the InjectManifest plugin to generate a service worker.
+      new InjectManifest({
+        swSrc: './src-sw.js', // Source path for service worker.
+        swDest: 'src-sw.js', // Destination path for service worker.
+      }),
+      // Generates a manifest.json file for the PWA.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple and lightweight text editor for the web.',
+        background_color: '#5dafdf',
+        theme_color: '#5dafdf',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
     module: {
+      // Rules for the modules (configure loaders, parser options, etc.).
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
