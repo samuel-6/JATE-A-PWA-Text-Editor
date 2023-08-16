8da0b589c180b117c2f82b919a7250f5d3ecc966
Azura <saamueel64@outlook.es>
2023-08-15 18:23:08 -0600

    Enhanced the PWA installation flow with improved event handling and install button logic. Implemented the logic for the 'beforeinstallprompt' and 'appinstalled'  events, and created the logic to display and hide the install button.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,44 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// This event will be triggered before the app is installed.
+window.addEventListener('beforeinstallprompt', (event) => {
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // Saves the event.
+    window.defferedPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Shows the install button.
+    butInstall.classList.toggle('hidden', false);
+
+});
+
+// This event will be triggered when the user clicks the install button.
+butInstall.addEventListener('click', async () => {
+
+    // Retrieves the deferred event.
+    const promptEvent = window.defferedPrompt;
+
+    // If there is no deferred event, exits the function.
+    if (!promptEvent) {
+
+        return;
+
+    }
+
+    // Prompts the user to install the app.
+    promptEvent.prompt();
+
+    // Clears the deffered event after prompting the user.
+    window.defferedPrompt = null;
+
+    // Hides the install button after the app has been successfully installed.
+    butInstall.classList.toggle('hidden', true);
+
+});
+
+// This event will be triggered after the app has been successfully installed.
+window.addEventListener('appinstalled', (event) => {
+
+    // Clears the saved event since the app has been installed.
+    window.defferedPrompt = null;
+
+});
